efaefe62a1c19c5252f633249c0170e82d736ef2
Codingdn <danthedevnguyen@gmail.com>
2020-07-18 06:54:21 -0400

    comment box fixed and styled

--- src/Post.js
+++ src/Post.js
@@ -1,17 +1,48 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import "./Post.css";
 import Avatar from "@material-ui/core/Avatar";
+import { db } from "./firebase";
+import firebase from 'firebase'
 
-const Post = ({username, caption, imageUrl})=> {
+const Post = ({ user, postId, username, caption, imageUrl }) => {
   const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState([]);
+
+  useEffect(() => {
+    let unsubscribe;
+    if (postId) {
+      unsubscribe = db
+        .collection("posts")
+        .doc(postId)
+        .collection("comments")
+        .orderBy('timestamp', 'desc')
+        .onSnapshot((snapshot) => {
+          setComments(snapshot.docs.map((doc) => doc.data()));
+        });
+    }
+
+    return () => {
+      unsubscribe();
+    };
+  }, [postId]);
+
+
+  const postComment = (e) =>{
+    e.preventDefault();
+
+    db.collection("posts").doc(postId).collection("comments").add({
+      text: comment, 
+      username: user.displayName,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    setComment('');
+  }
+
   return (
     <div className="post">
       <div className="post__header">
         {/**header -> avater + username */}
-        <Avatar
-          className="post__avatar"
-          src="/static/image/avatar/1.jpg"
-        />
+        <Avatar className="post__avatar" src="/static/image/avatar/1.jpg" />
         <h3>{username}</h3>
       </div>
       {/**image */}
@@ -21,30 +52,33 @@ const Post = ({username, caption, imageUrl})=> {
       <h4 className="post__text">
         <strong>{username}:</strong> {caption}
       </h4>
+
+      <div className="post__comments">
+        {comments.map((comment)=>(
+          <p>
+            <strong>{comment.username}: </strong>{comment.text}
+          </p>
+        ))}
+        </div>
+      <form className="post__commentBox">
+        <input
+          className="post__input"
+          type="text"
+          placeholder="Add a comment..."
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+        <button
+          className="post__button"
+          disabled={!comment}
+          type="submit"
+          onClick={postComment}
+        >
+          Post
+        </button>
+      </form>
     </div>
   );
-}
-// const Post = ({imageUrl, username, caption})=> {
-//   return (
-//     <div className="post">
-//       <div className="post__header">
-//         {/**header -> avater + username */}
-//         <Avatar
-//           className="post__avatar"
-//           // alt={username}
-//           src="/static/image/avatar/1.jpg"
-//         />
-//         <h3>username</h3>
-//       </div>
-//       {/**image */}
-//       <img className="post__image" src={imageUrl} />
-
-//       {/**userane + caption */}
-//       <h4 className="post__text">
-//         <strong>username:</strong> caption
-//       </h4>
-//     </div>
-//   );
-// }
+};
 
 export default Post;
